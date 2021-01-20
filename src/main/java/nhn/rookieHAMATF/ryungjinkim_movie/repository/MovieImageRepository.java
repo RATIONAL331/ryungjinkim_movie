700e5b0c6086608bf243ad958cdca505be479728
RyungJinKim <rational331@gmail.com>
2021-01-20 22:01:40 +0900

    [Repository] MovieImage 레포지토리 인터페이스 생성 및 테스트 #6

---
+++ src/main/java/nhn/rookieHAMATF/ryungjinkim_movie/repository/MovieImageRepository.java
@@ -0,0 +1,7 @@
+package nhn.rookieHAMATF.ryungjinkim_movie.repository;
+
+import nhn.rookieHAMATF.ryungjinkim_movie.entity.MovieImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieImageRepository extends JpaRepository<MovieImage, Long> {
+}
