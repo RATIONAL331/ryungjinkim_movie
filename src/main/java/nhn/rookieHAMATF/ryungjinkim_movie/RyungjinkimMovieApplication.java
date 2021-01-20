89115628ae59b39ac6b95931ebe299f0ac1f9283
RyungJinKim <rational331@gmail.com>
2021-01-20 21:26:59 +0900

    [Entity] BaseEntity 정의

--- src/main/java/nhn/rookieHAMATF/ryungjinkim_movie/RyungjinkimMovieApplication.java
+++ src/main/java/nhn/rookieHAMATF/ryungjinkim_movie/RyungjinkimMovieApplication.java
@@ -2,8 +2,10 @@ package nhn.rookieHAMATF.ryungjinkim_movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RyungjinkimMovieApplication {
 
 	public static void main(String[] args) {
