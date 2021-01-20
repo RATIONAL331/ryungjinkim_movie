786e94163474fdf2fdcf86f54f6ea7b3ee5b7bcc
RyungJinKim <rational331@gmail.com>
2021-01-20 10:13:39 +0900

    [FIRST COMMIT] movie review web first commit

---
+++ src/main/java/nhn/rookieHAMATF/ryungjinkim_movie/RyungjinkimMovieApplication.java
@@ -0,0 +1,13 @@
+package nhn.rookieHAMATF.ryungjinkim_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyungjinkimMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyungjinkimMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhn/rookieHAMATF/ryungjinkim_movie/RyungjinkimMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package nhn.rookieHAMATF.ryungjinkim_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyungjinkimMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
